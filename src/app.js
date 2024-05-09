4236a9e2fa08df0e574502a5f1b0a8084b5345cf
Prashant062 <prashantrawat786216@gmail.com>
2024-05-09 14:20:43 +0530

    all setup configured

--- src/app.js
+++ src/app.js
@@ -14,4 +14,13 @@ app.use(express.urlencoded({extended:true, limit:"16kb"}))
 app.use(express.static("public"))
 
 
+//import Routes
+
+import userRouter from "./routes/user.routes.js"
+
+
+// Routes declearation
+
+app.use("/api/v1/users" , userRouter)
+ 
 export { app }
